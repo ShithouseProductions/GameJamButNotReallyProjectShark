05f1be2b5d180f84c1e3094c60dd7bf75dde5734
Falkor <tobaksduo@gmail.com>
2020-10-17 12:41:30 +0200

    Public velocity variables, inventory UI

---
+++ ProjectSharkGameJamButNotReally/Assets/Scripts/Manger/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [Header("Setup")]
+    private GameObject cam;
+    private GameObject player;
+    void Start()
+    {
+        cam = gameObject;
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        //cam.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
