9a4a2bf1ae1486e8475732205e1959792eee354f
Falkor <tobaksduo@gmail.com>
2020-10-17 18:23:44 +0200

    enemy hunt but shit?

--- ProjectSharkGameJamButNotReally/Assets/Scripts/Enemy/Enemy.cs
+++ ProjectSharkGameJamButNotReally/Assets/Scripts/Enemy/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Velocity")]
+    public float velocity;
+
 
     [Header("Tools")]
     private float deltaX;
@@ -11,10 +14,12 @@ public class Enemy : MonoBehaviour
 
     [Header("GameObjects")]
     private GameObject player;
+    private Rigidbody2D rb;
 
     void Start()
     {
         player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     
@@ -23,6 +28,16 @@ public class Enemy : MonoBehaviour
         deltaX = player.transform.position.x - transform.position.x;
         deltaY = player.transform.position.y - transform.position.y;
 
+        //transform.Translate(Vector2.MoveTowards(transform.position, player.transform.position, 3) * velocity * Time.deltaTime);
+
+        Vector3 dir = player.transform.position - transform.position;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        rb.rotation = angle;
+        dir.Normalize();
+
+        rb.MovePosition(transform.position + (dir * velocity * Time.deltaTime));
         
     }
 }
