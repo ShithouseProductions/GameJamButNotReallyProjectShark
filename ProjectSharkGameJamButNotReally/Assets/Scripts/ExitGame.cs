bd7379ce8f0d6572f229bc967825f1a602c0e4a6
Sharkified Creator <1693useroscar@gmail.com>
2020-10-18 17:24:28 +0200

    Cool Scences.wav

---
+++ ProjectSharkGameJamButNotReally/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+   public void doExitGame()
+{
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+        print("Kevin");
+} 
+
+}
+
+
+
+
