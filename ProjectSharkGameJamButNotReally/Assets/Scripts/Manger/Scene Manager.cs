49b98c9735bf8ed20d352a9f30a91481ef5df294
Sharkified Creator <1693useroscar@gmail.com>
2020-10-18 16:18:55 +0200

    hup

---
+++ ProjectSharkGameJamButNotReally/Assets/Scripts/Manger/Scene Manager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager1 : MonoBehaviour
+{
+    void Start()
+    {
+        ScenceManager.LoadScene("Tutorial 1", LoadSceneMode.Additive);
+    }
+}
