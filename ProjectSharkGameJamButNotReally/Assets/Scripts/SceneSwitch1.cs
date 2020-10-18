2b1dabf7e504c2bef41b469529776624be652608
Sharkified Creator <1693useroscar@gmail.com>
2020-10-18 16:51:49 +0200

    Cool

---
+++ ProjectSharkGameJamButNotReally/Assets/Scripts/SceneSwitch1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch1 : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            print("yo!");
+        }
+        
+    }
+}
