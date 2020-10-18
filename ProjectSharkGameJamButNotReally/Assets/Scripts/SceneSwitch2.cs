e69ac097a6dd8b21547044418e8f708471285e3c
Sharkified Creator <1693useroscar@gmail.com>
2020-10-18 17:02:47 +0200

    Awesome

--- ProjectSharkGameJamButNotReally/Assets/Scripts/SceneSwitch2.cs
+++ ProjectSharkGameJamButNotReally/Assets/Scripts/SceneSwitch2.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneSwitch1 : MonoBehaviour
+public class SceneSwitch2 : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.name == "Player")
         {
-            SceneManager.LoadScene ("Tutorial 2");
+            SceneManager.LoadScene("Tutorial 3");
         }
-        
+
     }
 }
