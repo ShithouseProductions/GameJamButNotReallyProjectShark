6fca1e04e76797e2af66ee9d315591b0ffdc3ce6
Falkor <tobaksduo@gmail.com>
2020-10-18 18:44:59 +0200

    game generation

--- ProjectSharkGameJamButNotReally/Assets/Scripts/Tools/ENTERROOM.cs
+++ ProjectSharkGameJamButNotReally/Assets/Scripts/Tools/ENTERROOM.cs
@@ -4,20 +4,58 @@ using UnityEngine;
 
 public class ENTERROOM : MonoBehaviour
 {
+
     [Header("SPAWN")]
     public GameObject doorlocked;
     private bool completed = false;
+    private bool active;
+
+
+    [Header("enemy")]
+    public GameObject[] enemy = new GameObject[0];
+    private GameObject temp;
+
 
     void Start()
     {
 
     }
 
+    private void Update()
+    {
+        if(active)
+        {
+            if(transform.childCount == 0)
+            {
+                Destroy(temp);
+                Destroy(gameObject);
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player")
         {
-            Instantiate(doorlocked, transform.position, Quaternion.identity);
+            active = true;
+            temp = Instantiate(doorlocked, transform.position, Quaternion.identity);
+            SpawnEnemy();
+        }
+    }
+
+
+    private void SpawnEnemy()
+    {
+        int rand = Random.Range(0, 6);
+
+        for(int i = 0; i < rand; i++)
+        {
+            int posX = Random.Range(2, 20);
+            int posY = Random.Range(2, 10);
+
+            int enem = Random.Range(0, enemy.Length);
+
+            Instantiate(enemy[enem], transform.position + new Vector3(posX, posY, 0), Quaternion.identity, transform);
         }
     }
 
